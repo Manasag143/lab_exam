2e33ff2a84100a3cdb913b9621876b00a8658153
manasag143 <manasagrawal143@gmail.com>
2024-07-12 09:28:41 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,5 @@
+print("Please enter two numbers to find its addition")
+a= int(input("please enter first number="))
+b= int(input("please enter second number="))
+c = a+b
+print("The total of both the number = ",c)
